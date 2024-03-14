f7d0aa8f7e210043a685cc4aedec81f336a99fca
Jonathan Jayet <github@jayet.io>
2024-03-14 23:54:04 +0100

    readd non-dead code :)

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,8 @@
 import * as S from '@effect/schema/Schema'
 
 const Email = S.string.pipe(S.pattern(/^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i))
+export const Token = S.string.pipe(S.brand('Token'))
+export type Token = S.Schema.Type<typeof Token>
 
 export class GetTokenError extends S.TaggedError<GetTokenError>()('GetTokenError', {
   reason: S.string,
